72b4212cb9ec5270cf83fedc8ec75cb4bbb5d154
Xavier Lyu <xavier.lyu@protonmail.com>
2022-06-26 17:58:02 +0800

    Initial commit

---
+++ example/dio_cache_interceptor_sembast_store_example.dart
@@ -0,0 +1,6 @@
+import 'package:dio_cache_interceptor_sembast_store/dio_cache_interceptor_sembast_store.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
